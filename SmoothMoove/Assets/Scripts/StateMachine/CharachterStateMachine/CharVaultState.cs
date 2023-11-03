d3c015323383adee60ebbc5ffe6eb379b6efcb32
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-11-03 13:01:58 +0100

    fix and test

--- SmoothMoove/Assets/Scripts/StateMachine/CharachterStateMachine/CharVaultState.cs
+++ SmoothMoove/Assets/Scripts/StateMachine/CharachterStateMachine/CharVaultState.cs
@@ -12,7 +12,6 @@ public class CharVaultState : CharBaseState
         Debug.Log("Vault ENTER");
         Ctx.IsVaulted = true;
 
-        Ctx.PlayerAnimator.SetBool("Vault", true);
 
         float yOffset = Ctx.VaultObj.GetComponent<Renderer>().bounds.max.y + 1f;
         float xOffset = Mathf.Abs(Ctx.transform.forward.x) > Mathf.Abs(Ctx.transform.forward.z) ? (Ctx.VaultObj.transform.position.x - Ctx.transform.position.x) : 0f;
@@ -20,6 +19,7 @@ public class CharVaultState : CharBaseState
 
         Vector3 newPosition = new Vector3(Ctx.transform.position.x + xOffset, yOffset, Ctx.transform.position.z + zOffset);
         Ctx.transform.position = newPosition;
+        Ctx.PlayerAnimator.SetBool("Vault", true);
     }
 
     public override void ExitState()
