b32004e0ae0f4d2bb6cbe1e770349d343b9450f6
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2023-11-09 15:43:21 +0100

    stuff and things

--- SmoothMoove/Assets/Scenes/MelihStuff/BulletScript.cs
+++ SmoothMoove/Assets/Scenes/MelihStuff/BulletScript.cs
@@ -6,8 +6,21 @@ public class BulletScript : MonoBehaviour
 {
     public Vector3 travelDirection;
 
+    [SerializeField] float lifetime;
+
+    private void Start()
+    {
+        StartCoroutine(LifeTime());
+    }
+
     void Update()
     {
         transform.Translate(travelDirection * 50 * Time.deltaTime);
+    }
+
+    IEnumerator LifeTime()
+    {
+        yield return new WaitForSeconds(lifetime);
+        Destroy(this.gameObject);
     }
 }
